edfa011f5821556507865c9c4eed267d9e9e6dd3
Philip Meshach <philipmeshach.pm@gmail.com>
2022-09-12 20:13:11 +0530

    final commit

--- src/main/java/com/jap/validator/CredentialsValidator.java
+++ src/main/java/com/jap/validator/CredentialsValidator.java
@@ -1,5 +1,6 @@
 package com.jap.validator;
 
 public interface CredentialsValidator {
+    String validator(String userID,String password);
 
 }
--- src/main/java/com/jap/validator/CredentialsValidatorImpl.java
+++ src/main/java/com/jap/validator/CredentialsValidatorImpl.java
@@ -2,7 +2,17 @@ package com.jap.validator;
 
 public class CredentialsValidatorImpl {
     public String validateUser(String name, String password) {
-        return "";
+        CredentialsValidator credentialsValidator = ((userId, password1) -> {
+            if (name.equals("admin") && password.equals("1234")) {
+                return "valid user";
+
+            } else {
+                return "invalid user";
+            }
+
+        });
+
+        return credentialsValidator.validator(name, password);
     }
 
 
